d1b20ecf9c2b77fd6e3f61bcbeefb3f07cf12aaf
shashikant <pshashikant1998@gmail/com>
2023-10-22 18:09:26 +0530

    added props validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Button from './Components/Button'
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <div>
+        <Button primary secondary >Primary</Button>
+      </div>
+      <div>
+        <Button>Secondary</Button>
+      </div>
+      <div>
+        <Button>Danger</Button>
+      </div>
+      <div>
+        <Button>Warning</Button>
+      </div></>
   )
 }
 
